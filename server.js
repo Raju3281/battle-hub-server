569659a5244b169aa031dba03479de5bfb782e1c
Raju3281 <140163937+Raju3281@users.noreply.github.com>
2026-01-21 15:04:27 +0530

    check change

--- server.js
+++ server.js
@@ -60,4 +60,4 @@ app.use((req, res, next) => {
 app.get("/", (req, res) => res.send("BattleHub API running..."));
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`🚀 Server running on port ...... ${PORT}`));
